e6ad7ef6baf942cf088854036da777910bad4271
Shivam Pandey <shivamraj472@gmail.com>
2020-06-02 13:27:25 +0530

    Add files via upload

---
+++ Assignment1/src/Question3.java
@@ -0,0 +1,17 @@
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("    TTTTTTTT        K      K");
+		System.out.println("       T            K    K");
+		System.out.println("       T            K  K");
+		System.out.println("       T            K  ");
+		System.out.println("       T            K  K");
+		System.out.println("       T            K    K");
+		System.out.println("       T            K      K");
+		
+
+	}
+
+}
---
+++ Assignment1/src/Question9.java
@@ -0,0 +1,13 @@
+
+public class Question9 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(2 + "bc");
+		System.out.println(2 + 3 + "bc");
+		System.out.println((2+3) + "bc");
+		System.out.println("bc" + (2+3));
+		System.out.println("bc" + 2 + 3);
+	}
+
+}
